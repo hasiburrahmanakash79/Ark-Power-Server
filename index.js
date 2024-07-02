3e7f7e80e766f62af8f31c7c115967588241656c
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-07-02 14:46:41 +0600

    make user delete

--- index.js
+++ index.js
@@ -61,6 +61,14 @@ async function run() {
       res.send(result);
     });
 
+    // delete user from database
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    });
+
     //make admin
     app.patch("/users/:id", async (req, res) => {
       const id = req.params.id;
